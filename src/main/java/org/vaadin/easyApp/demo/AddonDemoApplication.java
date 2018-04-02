7292f61921f7a1db940a8199df3a1ad2770bb422
guillaume Rousseau <igolus@gmail.com>
2018-04-02 21:23:37 +0200

    dev

--- src/main/java/org/vaadin/easyApp/demo/AddonDemoApplication.java
+++ src/main/java/org/vaadin/easyApp/demo/AddonDemoApplication.java
@@ -3,14 +3,16 @@ package org.vaadin.easyApp.demo;
 import java.util.Collections;
 
 import org.vaadin.easyapp.EasyAppBuilder;
-import org.vaadin.easyapp.EasyAppMainView;
+import org.vaadin.easyapp.ui.ViewWithToolBar;
+import org.vaadin.easyapp.util.ActionContainerBuilder;
 
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Image;
-import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -21,7 +23,7 @@ import com.vaadin.ui.VerticalLayout;
 @Theme("mytheme")
 public class AddonDemoApplication extends UI {
 
-    private EasyAppMainView easyAppMainView;
+    private ViewWithToolBar easyAppMainView;
 	//private static AddonDemoApplication instance;
 
 	@Override
@@ -34,55 +36,31 @@ public class AddonDemoApplication extends UI {
 		image.setWidth(50, Unit.PIXELS);
 		image.setHeight(50, Unit.PIXELS);
 		
-		easyAppMainView = new EasyAppBuilder(Collections.singletonList("org.vaadin.easyApp.demo.view"))
-        	//.withTopBar()
-			.withNavigationIcon(image)
-//			.withTopBarIcon(image)
-//        	.withTopBarStyle("topBannerBackGround")
-//        	.withSearchCapabilities( (searchValue) -> search(searchValue) , FontAwesome.SEARCH)
-//        	.withLogingCapabilities( 
-//        			(user, password) -> loginAttempt(user, user),
-//        			() -> logout()
-//			)
-//        	.withLogingUserText("login:")
-//        	.withLogingPassWordText("password:")
-//        	.withLoginCaption("Please login to access the application. (test@test.com/passw0rd)")
-//        	.withLoginTextStyle("userLogged")
-//        	.withLoginErroText("Bad credentials")
-//        	.withLoginErrotLabelStyle("error")
-//        	.withBreadcrumb()
-//        	.withBreadcrumbStyle("breadcrumbStyle")
-        	//.withButtonLinkStyleInBreadCrumb(BaseTheme.BUTTON_LINK)
-        	//.withLoginPopupLoginStyle("propupStyle")
-        	.build(this);
-	
+		EasyAppBuilder easyAppBuilder = new EasyAppBuilder(Collections.singletonList("org.vaadin.easyApp.demo.view"));
+		easyAppBuilder.withNavigationIcon(image);
+		easyAppBuilder.withNavigationStlyle("Nav", "Selected");
+		easyAppBuilder.withTopBarIcon(image);
 		
+		
+		ActionContainerBuilder actionContainerBuilder = new ActionContainerBuilder(null)
+				.addButton("Test", VaadinIcons.MINUS, null , this::always, this::test);
+		actionContainerBuilder.withStyleNam("TopBar");
+		actionContainerBuilder.addImageIcon(image);
+		easyAppBuilder.withActionContainer(actionContainerBuilder.build());
+	
+		easyAppMainView = easyAppBuilder.build(this);
 		layout.addComponents(easyAppMainView);
-        
-		//easyAppMainView.getTopBar().setStyleName("topBannerBackGround");
         
         setContent(layout);
         
     }
-
-	private void logout() {
-		Notification.show("Logged out");
+	
+	public void test(ClickEvent event) {
+		
 	}
-
-	private String loginAttempt(String user, String password) {
-		if (user.equals("admin")) {
-			return " *** logged as " + user + " *** ";
-		}
-		//null means error
-		return null;
+	
+	public boolean always() {
+		return true;
 	}
-
-	private void search(String searchValue) {
-    	Notification.show("Search for : " + searchValue + easyAppMainView.getNavigator().getCurrentView());
-	}
-
-//	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-//    @VaadinServletConfiguration(ui = AddonDemoApplication.class, productionMode = false)
-//    public static class MyUIServlet extends VaadinServlet {
-//    }
+	
 }
